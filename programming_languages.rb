937ce6ae09782eb3038a59298853747fad66e5f9
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-06 21:13:09 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,12 +9,12 @@ def reformat_languages(languages)
 
   keys_array = []
   new_hash.each do |key, value|
-    languages.each_key do |lang_key| 
+    languages.each_key do |lang_key|
       if languages[lang_key].include?(key)
         keys_array << lang_key
       end
-      new_hash[:style]=keys_array
     end
+    new_hash[:style]=keys_array
   end
   return new_hash
 end
