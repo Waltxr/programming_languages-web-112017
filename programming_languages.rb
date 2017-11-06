b2105af083ea244f047f8c536d5f3c32479c68ab
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-06 19:33:05 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,7 @@
 def reformat_languages(languages)
   new_hash = {}
 
-  languages.map do |key, value|  
-    new_hash[key][value]
+  languages.map do |key, value|
+    return new_hash[key]=value
   end
 end
